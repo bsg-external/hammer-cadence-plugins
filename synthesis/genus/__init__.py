2ec58d3108e55879d7082d6c87f0cde29c7ddd7f
Scott Davidson <scottdavidson1992@gmail.com>
2022-12-31 01:57:15 -0800

    Adding setting to synthesis to help avoid x-pessimisim issues in gate level simulations

--- synthesis/genus/__init__.py
+++ synthesis/genus/__init__.py
@@ -399,6 +399,8 @@ def genus_global_settings(ht: HammerTool) -> bool:
     # Generic Settings
     verbose_append("set_db hdl_error_on_blackbox true")
     verbose_append("set_db max_cpus_per_server {}".format(ht.get_setting("vlsi.core.max_threads")))
+    # BSG Scott - Infer synchronous resets (helps avoid x-pessimisim issues)
+    verbose_append("set_db hdl_auto_sync_set_reset true")
 
     return True
 
